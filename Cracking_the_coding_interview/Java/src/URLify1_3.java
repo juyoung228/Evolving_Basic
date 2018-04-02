46ede0c1e42a6e5314b315c46f5c614532b9e764
juyoung228 <juyoung228@gmail.com>
2018-04-02 20:44:16 +0900

    add new files

--- Cracking_the_coding_interview/Java/src/URLify1_3.java
+++ Cracking_the_coding_interview/Java/src/URLify1_3.java
@@ -11,28 +11,28 @@ public class URLify1_3 {
 		}
 		
 		index = trueLength + spaceCount * 2;
-		char[] str_new = new char[index];
 		//https://stackoverflow.com/questions/26558741/what-does-strnewlength-0-mean
 		if (trueLength <str.length) str[trueLength] = '\0';
 		for (i = trueLength -1; i >=0; i--)
 		{
 			if (str[i] == ' ') {
-				str_new[index - 1] = '0';
-				str_new[index - 2] = '2';
-				str_new[index - 3] = '%';
+				str[index - 1] = '0';
+				str[index - 2] = '2';
+				str[index - 3] = '%';
 				index = index -3;
 			}else{
-				str_new[index -1] = str[i];
+				str[index -1] = str[i];
 				index--;
 			}
 		}
 		
-		System.out.println(str_new);
+		System.out.println(str);
 	}
 
 	
 	public static void main(String args[])
 	{
-		replaceSpaces("Mr John Smith".toCharArray(), 13);
+		// Assume string has sufficient free space at the end
+		replaceSpaces("Mr John Smith    ".toCharArray(), 13);
 	}
 }
