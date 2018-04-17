0fe67ddbaec9731dd0538b3efa83aea14ec69262
juyoung228 <juyoung228@gmail.com>
2018-04-17 19:30:54 +0900

    added LinkedList2 which has tail attribute

--- Cracking_the_coding_interview/python/utils/linked_list.py
+++ Cracking_the_coding_interview/python/utils/linked_list.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 class Node:
     def __init__(self, item):
         self.val = item
@@ -63,3 +66,81 @@ class LinkedList:
     def num_of_list(self):
         print("The list has", self.num, "items")
         return self.num
+
+'''
+The following classes are added.
+There is tail in these classes.
+ref: https://github.com/careercup/CtCI-6th-Edition-Python/blob/e6bc732588601d0a98e5b1bc44d83644b910978d/Chapter2/LinkedList.py
+'''
+class LinkedListNode:
+
+    def __init__(self, value, nextNode=None, prevNode=None):
+        self.value = value
+        self.next = nextNode
+        self.prev = prevNode
+
+    def __str__(self):
+        return str(self.value)
+
+
+class LinkedList2:
+
+    def __init__(self, values=None):
+        self.head = None
+        self.tail = None
+        if values is not None:
+            self.add_multiple(values)
+
+    def __iter__(self):
+        current = self.head
+        while current:
+            yield current
+            current = current.next
+
+    def __str__(self):
+        values = [str(x) for x in self]
+        return ' -> '.join(values)
+
+    def __len__(self):
+        result = 0
+        node = self.head
+        while node:
+            result += 1
+            node = node.next
+        return result
+
+    def add(self, value):
+        if self.head is None:
+            self.tail = self.head = LinkedListNode(value)
+        else:
+            self.tail.next = LinkedListNode(value)
+            self.tail = self.tail.next
+        return self.tail
+
+    def add_to_beginning(self, value):
+        if self.head is None:
+            self.tail = self.head = LinkedListNode(value)
+        else:
+            self.head = LinkedListNode(value, self.head)
+        return self.head
+
+    def add_multiple(self, values):
+        for v in values:
+            self.add(v)
+
+    def generate(self, n, min_value, max_value):
+        self.head = self.tail = None
+        for i in range(n):
+            self.add(randint(min_value, max_value))
+        return self
+
+
+class DoublyLinkedList(LinkedList):
+
+    def add(self, value):
+        if self.head is None:
+            self.tail = self.head = LinkedListNode(value, None, self.tail)
+        else:
+            self.tail.next = LinkedListNode(value)
+            self.tail = self.tail.next
+        return self
