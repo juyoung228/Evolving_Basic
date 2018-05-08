f3e773093427aacfe5942bcbe4e9aa5a954a696b
juyoung228 <juyoung228@gmail.com>
2018-05-09 00:51:48 +0900

    update the file

--- Cracking_the_coding_interview/Java/src/Sum2_5.java
+++ Cracking_the_coding_interview/Java/src/Sum2_5.java
@@ -38,22 +38,98 @@ public class Sum2_5 {
 		return result;
 	}
 	
-	public static int linkedListToInt(LinkedListNode node) {
+
+	public static int linkedListToInt_1(LinkedListNode node) {
 		int value = 0;
 		if (node.next != null) {
-			value = 10 * linkedListToInt(node.next);
+			
+			value = 10 * linkedListToInt_1(node.next);
 		}
 		return value + node.data;
 	}	
 	
+	
+	
+	public static class PartialSum {
+		public LinkedListNode sum = null;
+		public int carry = 0;
+	}
+	
+	private static int length(LinkedListNode l) {
+		if (l == null) {
+			return 0;
+		} else {
+			return 1 + length(l.next);
+		}
+	}
+	
+	private static PartialSum addListsHelper(LinkedListNode l1, LinkedListNode l2) {
+		if (l1 == null && l2 == null) {
+			PartialSum sum = new PartialSum();
+			return sum;
+		}
+		PartialSum sum = addListsHelper(l1.next, l2.next);
+		int val = sum.carry + l1.data + l2.data;
+		LinkedListNode full_result = insertBefore(sum.sum, val % 10);
+		sum.sum = full_result;
+		sum.carry = val / 10;
+		return sum;
+	}
+	
+	private static LinkedListNode addLists_2(LinkedListNode l1, LinkedListNode l2) {
+		int len1 = length(l1);
+		int len2 = length(l2);
+		if (len1 < len2) {
+			l1 = padList(l1, len2 - len1);
+		} else {
+			l2 = padList(l2, len1 - len2);
+		}
+		PartialSum sum = addListsHelper(l1, l2);
+		if (sum.carry == 0) {
+			return sum.sum;
+		} else {
+			LinkedListNode result = insertBefore(sum.sum, sum.carry);
+			return result;
+		}
+	}	
+	
+	private static LinkedListNode padList(LinkedListNode l, int padding) {
+		LinkedListNode head = l;
+		for (int i = 0; i < padding; i++) {
+			head = insertBefore(head, 0);
+		}
+		return head;
+	}
+	
+	private static LinkedListNode insertBefore(LinkedListNode list, int data) {
+		LinkedListNode node = new LinkedListNode(data);
+		if (list != null) {
+			node.next = list;
+		}
+		return node;
+	}
+	
+	public static int linkedListToInt(LinkedListNode node) {
+		int value = 0;
+		while (node != null) {
+			value = value * 10 + node.data;
+			node = node.next;
+		}
+		return value;
+	}	
+	
+	
 	public static void main(String[] args) {
+		
+		
+		//test code for addLists_1
 		LinkedListNode lA1 = new LinkedListNode(9, null, null);
 		LinkedListNode lA2 = new LinkedListNode(9, null, lA1);
 		LinkedListNode lA3 = new LinkedListNode(9, null, lA2);
 		
 		LinkedListNode lB1 = new LinkedListNode(1, null, null);
 		LinkedListNode lB2 = new LinkedListNode(0, null, lB1);
-		LinkedListNode lB3 = new LinkedListNode(0, null, lB2);	
+		LinkedListNode lB3 = new LinkedListNode(0, null, lB2);
 		
 		LinkedListNode list3 = addLists_1(lA1, lB1, 0);
 		
@@ -61,12 +137,39 @@ public class Sum2_5 {
 		System.out.println("+ " + lB1.printForward());			
 		System.out.println("= " + list3.printForward());	
 		
-		int l1 = linkedListToInt(lA1);
-		int l2 = linkedListToInt(lB1);
-		int l3 = linkedListToInt(list3);
+		int l1 = linkedListToInt_1(lA1);
+		int l2 = linkedListToInt_1(lB1);
+		int l3 = linkedListToInt_1(list3);
 		
 		System.out.print(l1 + " + " + l2 + " = " + l3 + "\n");
-		System.out.print(l1 + " + " + l2 + " = " + (l1 + l2));		
+		System.out.print(l1 + " + " + l2 + " = " + (l1 + l2));	
+		
+		
+		
+		System.out.println("\n===================================");
+		
+		//test code for addLIsts_2
+		//assume that the liknedlist is sorted in not reversed way
+		
+		
+		LinkedListNode l2A1 = new LinkedListNode(9, null, null);
+		LinkedListNode l2A2 = new LinkedListNode(9, null, l2A1);
+		LinkedListNode l2A3 = new LinkedListNode(9, null, l2A2);
+		
+		LinkedListNode l2B1 = new LinkedListNode(1, null, null);	
+		
+		LinkedListNode list3_2 = addLists_2(l2A1, l2B1);
+		
+		System.out.println("  " + l2A1.printForward());		
+		System.out.println("+ " + l2B1.printForward());			
+		System.out.println("= " + list3_2.printForward());	
+		
+		int l21 = linkedListToInt(l2A1);
+		int l22 = linkedListToInt(l2B1);
+		int l23 = linkedListToInt(list3_2);
+		
+		System.out.print(l21 + " + " + l22 + " = " + l23 + "\n");
+		System.out.print(l21 + " + " + l22 + " = " + (l21 + l22));		
 	}
 
 }
