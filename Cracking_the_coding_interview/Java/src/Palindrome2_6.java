4a93a0cd9f519341ab154ed40afe47595b729bd3
juyoung228 <juyoung228@gmail.com>
2018-05-15 01:01:03 +0900

    update the file

--- Cracking_the_coding_interview/Java/src/Palindrome2_6.java
+++ Cracking_the_coding_interview/Java/src/Palindrome2_6.java
@@ -1,4 +1,6 @@
 
+import utils.LinkedListNode;
+
 public class Palindrome2_6 {
 
 	/**
@@ -7,4 +9,55 @@ public class Palindrome2_6 {
 	 * 
 	 **/
 	
+	static boolean isPalindrome(LinkedListNode head) {
+		LinkedListNode reversed = reverseAndClone(head);
+		return isEqual(head, reversed);
+	}
+	
+	static LinkedListNode reverseAndClone(LinkedListNode node){
+		LinkedListNode head = null;
+		while (node != null) {
+			LinkedListNode n = new LinkedListNode(node.data); //copy
+			n.next = head;
+			head = n;
+			node = node.next;
+		}
+		return head;
+	}
+	
+	static boolean isEqual(LinkedListNode one, LinkedListNode two) {
+		while (one != null && two != null) {
+			if (one.data != two.data) {
+				return false;
+			}
+			one = one.next;
+			two = two.next;
+		}
+		return one == null && two == null;
+	}
+	
+	
+	public static void main(String[] args) {
+		int length = 9;
+		LinkedListNode[] nodes = new LinkedListNode[length];
+		//make each node 0 1 2 3 4 3 2 1 0
+		for (int i = 0; i < length; i++) {
+			nodes[i] = new LinkedListNode(i >= length / 2 ? length - i - 1 : i, null, null);
+		}
+		
+		//connect the nodes back and forth
+		for (int i = 0; i < length; i++) {
+			if (i < length - 1) {
+				nodes[i].setNext(nodes[i + 1]);
+			}
+			if (i > 0) {
+				nodes[i].setPrevious(nodes[i - 1]);
+			}
+		}
+		 //nodes[length - 2].data = 9; // Uncomment to ruin palindrome
+		
+		LinkedListNode head = nodes[0];
+		System.out.println(head.printForward());
+		System.out.println(isPalindrome(head));
+	}
 }
