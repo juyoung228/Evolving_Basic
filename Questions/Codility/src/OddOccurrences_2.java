11bd5904e33c2c9c6592390da8940543a9b0a8ff
juyoung228 <juyoung228@gmail.com>
2018-05-20 01:12:30 +0900

    update the file

--- Questions/Codility/src/OddOccurrences_2.java
+++ Questions/Codility/src/OddOccurrences_2.java
@@ -1,4 +1,30 @@
 
+// time complexity: O(N) or O(N*log(N))
+	import java.util.HashMap;
+	import java.util.Arrays;
+	import java.util.List;
+	import java.util.Iterator;
+
 public class OddOccurrences_2 {
 
+	    public int solution(int[] A) {
+	        // write your code in Java SE 8
+	        HashMap <Integer, Integer> map = new HashMap<Integer, Integer>();
+	        for (int i = 0; i < A.length; i++){
+	            int val = A[i];
+	            if (map.containsKey(val)) {
+	             map.remove(val);
+	            }else{
+	             map.put(val, i);
+	            }
+	        }
+	        Iterator<Integer> iterator = map.keySet().iterator();
+	        
+	        int key = iterator.next();
+	        
+	        
+	        return key;
+	        
+	    }
 }
+
