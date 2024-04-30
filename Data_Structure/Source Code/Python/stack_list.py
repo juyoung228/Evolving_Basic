d3443ee6bdc6735ac3e1557f0cf304d63b72c4ee
JUYOUNG <juyoung228@gmail.com>
2024-04-30 22:56:56 +0900

    Create stack_list.py

---
+++ Data_Structure/Source Code/Python/stack_list.py
@@ -0,0 +1,14 @@
+#https://www.youtube.com/watch?v=7C9RgOcvkvo&t=225s
+stack = []
+
+stack.append[5]
+stack.append[2]
+stack.append[3]
+stack.append[7]
+stack.pop() #끝에서부터 삭제.
+stack.append(1)
+stack.append(4)
+stack.pop()
+
+print(stack)
+#[5,2,3,1]
