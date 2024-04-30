3ae02d1f3f460f99bf879ac9f3f3bde544f2c8dc
JUYOUNG <juyoung228@gmail.com>
2024-04-30 23:16:12 +0900

    Update stack_simple.py

--- Data_Structure/Source Code/Python/stack_simple.py
+++ Data_Structure/Source Code/Python/stack_simple.py
@@ -1,4 +1,6 @@
 #https://www.youtube.com/watch?v=7C9RgOcvkvo&t=225s
+#함수를 연속적으로 호출하면 컴퓨터 메모리 내부의 스택 프레임에 쌓입니다. 
+#그래서 스택을 사용해야 할 때 구현상 스택 라이브러리 대신에 재귀 함수를 이용하는 경우가 많습니다.
 stack = []
 
 stack.append[5]
